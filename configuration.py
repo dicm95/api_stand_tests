386a6f9b3c7e4305b21a9ed78fc7e712593fc42b
Daniela Contreras M <danicontrerasm@iMac-de-Daniela.lan>
2024-12-29 15:46:36 +0100

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f7a4da37-d53e-499e-93b6-a3b51cec4b1a.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
